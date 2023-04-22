1759075f0867fde9b266b9fc141cd29ccd09320e
Robert Kamau <parrotdevelopers254@gmail.com>
2023-04-22 14:09:35 +0300

    Probably the final commit for now. Might work add onto it later

--- myPortfolio1/home/urls.py
+++ myPortfolio1/home/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from home import views
 
+#Django admin customization
+admin.site.site_header = "Login To Robert Kamau AdminPort"
+admin.site.site_title = "Welcome to Developer Robert's AdminPort"
+admin.site.index_title = "Welcome Kiongozi"
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact', views.contact, name='contact'),
--- myPortfolio1/home/views.py
+++ myPortfolio1/home/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def contact(request):
+    if request.method=="POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        phone = request.POST["phone"]
+        desc = request.POST["desc"]
+        ins = Contact(name=name, phone=phone, email=email, desc=desc)
+        ins.save()
+        print("The data has been written to the db")
     return render(request, 'contact.html')
 
 def about(request):
